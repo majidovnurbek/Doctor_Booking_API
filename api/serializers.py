a932431910a605214862b13a75b969fbda987b9d
Nurbek <152709750+majidovnurbek@users.noreply.github.com>
2025-01-23 12:02:33 +0500

    Commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
-from .models import Doctor
+from .models import Doctor,User,News
+from django.conf import settings
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','avatar')
 
 
 class DoctorSerializer(serializers.ModelSerializer):
+    user =  UserSerializer()
     class Meta:
         model = Doctor
-        fields = ['specialization','experience','location','clinic_name','cunsultation_fee','is_consultation_fee','avaible_today']
+        fields = ['user','specialization','experience','location','clinic_name','cunsultation_fee','is_consultation_fee','avaible_today']
+
+class NewsSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    img=serializers.SerializerMethodField()
+    class Meta:
+        model = News
+        fields=['user','title','img','created_at']
+
+    def get_img(self,obj):
+        if obj.img:
+            return settings.BASE_URL + obj.img.url
+        return None
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from api.views import DoctorAPIView
+from api.views import DoctorAPIView, NewsAPIView
 
 urlpatterns = [
     path('doctor', DoctorAPIView.as_view(), name='doctors-list'),
+    path('news', NewsAPIView.as_view(), name='news-list'),
+    path('news/<int:pk>', NewsAPIView.as_view(), name='news-detail'),
     path('doctor/<int:pk>', DoctorAPIView.as_view(), name='doctors-detail'),
 ]
